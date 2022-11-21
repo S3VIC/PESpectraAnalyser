637e7d0391dd9a3cb7fb45d0284268923a26f6f4
S3VIC <62557969+S3VIC@users.noreply.github.com>
2022-11-21 13:49:04 +0100

    spectraAnalyzer-0.1
    Basic plotting with colorful signing specific signals being characteristic to a substance measured during Master thesis

---
+++ src/params/parameters.py
@@ -0,0 +1,22 @@
+# Plotting parameters
+X_RANGE_check_2 = (2700, 3100)
+X_RANGE_check_1 = (900, 1600)
+X_RANGE_full = (38, 3300)
+GRID_VISIBILITY = True
+FILENAME_EXTENSION = "CSV"
+Y_LABEL = "I [arb.uni.]"
+X_LABEL = "Raman shift [cm\u207B" + chr(0xB9) + "]"
+DERIVATIVE_Y_LABEL = "derivative value"
+
+PEAK_1 = (1062, 1065)
+PEAK_2 = (1130, 1133)
+PEAK_3 = (1170, 1174)
+PEAK_4 = (1295, 1296)
+PEAK_5 = (1382, 1370)
+PEAK_6 = (1415, 1418)
+PEAK_7 = (1440, 1440)
+PEAK_8 = (2848, 2865)
+PEAK_9 = (2883, 2932)
+PEAK_10 = (2932, 2932)
+
+###
